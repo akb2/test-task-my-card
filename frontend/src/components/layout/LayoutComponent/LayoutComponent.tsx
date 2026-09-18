2ce84cb3c8626f6f373f2a4194b7fb084915a289
akb2 <akb2-online@ya.ru>
2026-09-18 16:38:17 +0400

    Added page sctructure & content scroll

--- frontend/src/components/layout/LayoutComponent/LayoutComponent.tsx
+++ frontend/src/components/layout/LayoutComponent/LayoutComponent.tsx
@@ -1,10 +1,11 @@
 import { FC } from "react";
 import { HeaderComponent } from "@components/layout/HeaderComponent";
 import { LayoutComponentProps } from "./LayoutComponent.types";
+import styles from "./LayoutComponent.module.scss";
 
 export const LayoutComponent: FC<LayoutComponentProps> = ({ children }) => (
-  <>
+  <div className={styles.layout}>
     <HeaderComponent />
-    <div>{children}</div>
-  </>
+    <div className={styles.content}>{children}</div>
+  </div>
 );
