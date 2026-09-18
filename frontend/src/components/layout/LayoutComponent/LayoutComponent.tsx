fd255926785ef03bef0db10ad92f241bc70705ea
akb2 <akb2-online@ya.ru>
2026-09-18 17:07:32 +0400

    Added content styles

--- frontend/src/components/layout/LayoutComponent/LayoutComponent.tsx
+++ frontend/src/components/layout/LayoutComponent/LayoutComponent.tsx
@@ -6,6 +6,8 @@ import styles from "./LayoutComponent.module.scss";
 export const LayoutComponent: FC<LayoutComponentProps> = ({ children }) => (
   <div className={styles.layout}>
     <HeaderComponent />
-    <div className={styles.content}>{children}</div>
+    <div className={styles.content}>
+      <div className={styles.content__helper}>{children}</div>
+    </div>
   </div>
 );
